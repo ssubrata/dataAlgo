20462d515dac46b35ca6f9b5623049db4be80a2a
subrata roy <subratamail24@gmail.com>
2020-01-24 20:18:13 +0600

    'modifybubblesort'

--- DataStructure/Sorting.cs
+++ DataStructure/Sorting.cs
@@ -1,14 +1,23 @@
 namespace dataAlgo {
     public class Sorting {
-        public int[] BubbleSort (int[] array) {
-            for (var i = 0; i < array.Length; i++)
-                if (array[i] > array[i + 1]) {
-                    var temp = array[i + 1];
-                    array[i + 1] = array[i];
-                    array[i] = temp;
-
+        public void BubbleSort (int[] array) {
+            bool isSorted = false;
+            for (var i = 0; i < array.Length; i++) {
+                isSorted = true;
+                for (int j = 1; j < array.Length - i; j++) {
+                    if (array[j] < array[j - 1]) {
+                        Swap (array, j, j - 1);
+                        isSorted = false;
+                    }
+                    if (isSorted) return;
                 }
-            return array;
+            }
+        }
+
+        private void Swap (int[] array, int index, int index1) {
+            var temp = array[index1];
+            array[index1] = array[index];
+            array[index] = temp;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 
-namespace dataAlgo {
-    class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Welcome To Data Structure Test");
+namespace dataAlgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Data Structure Test");
             int[] array = { 4, 1, 5, 3, 9, 7, 10, 15, 11 };
-            Sorting sorting = new Sorting ();
-            var result = sorting.BubbleSort (array);
-            for (int i = 0; i < result.Length; i++) {
-                Console.WriteLine (result[i]);
+            Sorting sorting = new Sorting();
+            sorting.BubbleSort(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
             }
         }
     }
